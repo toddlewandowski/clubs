19bd4dad40af0d65696643c868f3d1ef73fcb04d
todd <todd.lewandowski@gmail.com>
2011-05-27 00:11:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clubs::Application.config.secret_token = '43264b3a8713e3082eaa4833c89ee6bb85485e5702b1570cf9e0e849858bd811db9ea5c9cb59a3eae934aa6849dea551d4dbc894713cc8834483569525170c8c'
